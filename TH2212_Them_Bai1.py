c57af9fd160fb77e04f5b01f259ba8799aa3d165
dungnq67khmt-debug <dung.nq.67khmt@ntu.edu.vn>
2025-12-22 14:29:25 +0700

    Create TH2212_Them_Bai1.py

---
+++ TH2212_Them_Bai1.py
@@ -0,0 +1,14 @@
+import math
+while True:
+    a=float(input("Nhập cạnh a:"))
+    b=float(input("Nhập cạnh b:"))
+    c=float(input("Nhập cạnh c:"))
+    if a+b>c or a+c>b or b+c>a:
+        p=a+b+c
+        ncv=p/2
+        s=math.sqrt(ncv*(ncv-a)*(ncv-b)*(ncv-c))
+        break
+print(round(p,2))
+print(round(s,2))
+
+
