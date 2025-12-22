b94566922beb793c1b755a2b8805f7f9e2c67fe6
dungnq67khmt-debug <dung.nq.67khmt@ntu.edu.vn>
2025-12-22 14:33:17 +0700

    Update TH2212_Them_Bai1.py

--- TH2212_Them_Bai1.py
+++ TH2212_Them_Bai1.py
@@ -3,7 +3,7 @@ while True:
     a=float(input("Nhập cạnh a:"))
     b=float(input("Nhập cạnh b:"))
     c=float(input("Nhập cạnh c:"))
-    if a+b>c or a+c>b or b+c>a:
+    if a+b>c and a+c>b and b+c>a:
         p=a+b+c
         ncv=p/2
         s=math.sqrt(ncv*(ncv-a)*(ncv-b)*(ncv-c))
