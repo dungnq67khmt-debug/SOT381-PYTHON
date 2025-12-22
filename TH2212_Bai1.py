6ce5fb15bdefb639db859ea37e5e901b22e797b6
dungnq67khmt-debug <dung.nq.67khmt@ntu.edu.vn>
2025-12-22 14:04:13 +0700

    Create TH2212_Bai1.py

---
+++ TH2212_Bai1.py
@@ -0,0 +1,9 @@
+h=float(input("Nhập chiều dài hcn:"))
+w=float(input("Nhập chiều rộng hcn:"))
+if h>=100 or w<=0:
+    print("vui lòng nhập lại")
+else:
+    C=(h+w)*2
+    S=h*w
+print("Chu vi hcn:",round(C))
+print("Diện tích hcn:",round(S))
